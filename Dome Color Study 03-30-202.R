e2c3663a6844e2b26399bf10d2f86470cafc7baf
supertime1 <37205904+supertime1@users.noreply.github.com>
2020-03-31 14:10:24 -0700

    changed a line

---
+++ Dome Color Study 03-30-202.R
@@ -0,0 +1,86 @@
+library(dplyr)
+library(tidyr)
+library(MLmetrics)#MAPE, MAE
+library(ggplot2)
+library(stringr)#String operations
+library(psych)#ICC
+library(formattable)#make a table
+
+#extract data
+setwd('C:/Users/57lzhang.US04WW4008/Desktop/GG - dome eval/Dome Color')
+temp<-list.files(pattern = "^PTek*")
+
+df <- data.frame()
+for (i in temp) {
+  tt<-str_split(i,'-')[[1]][3]
+  #extract activity type from each filename
+  activity <- str_split(tt, ' ')[[1]][3]
+  #extract dome color type from each filename
+  color_csv <- str_split(tt, ' ')[[1]][7]
+  color<-gsub(".csv", "", color_csv)
+  #extact dome type from each filename
+  type <- str_split(tt, ' ')[[1]][6]
+  
+  t <-read.csv(i, header = T)
+  t1 <- t%>%mutate(Activity = activity)%>%
+    mutate(Color = color)%>%
+    mutate(Type = type)
+  #select the first 3mins data, with excluding the first 30s
+  t1<-t1%>%slice(32:212)
+  
+  #combine all files into a big one
+  df <-bind_rows(t1,df)
+}
+
+#this will create a summarized table
+#pay attention to the order or HR, HR.1 in raw csv file, 
+#..where the 2nd argument in MAPE
+#..and MAE should be gold-standard
+
+#ICC function takes a single argument - dataframe, 
+#..where the order of HR,HR.1 doesn't matter
+df_sum <- df %>% 
+  drop_na(HR.1,HR)%>% 
+  group_by(Color, Activity, Type) %>% 
+  summarise(MAPE = round(MAPE(HR,HR.1)*100,1),
+            MAE = round(MAE(HR,HR.1),1),
+            Pearson_Corr = round(cor(HR,HR.1),2),
+            ICC = round(ICC(data.frame(HR,HR.1))$results[2,2],2))
+
+formattable(df_sum_act)
+
+#turn off the Activity group
+df_sum <- df %>% 
+  drop_na(HR.1,HR)%>% 
+  group_by(Color, Type) %>% 
+  summarise(MAPE = round(MAPE(HR,HR.1)*100,1),
+            MAE = round(MAE(HR,HR.1),1),
+            Pearson_Corr = round(cor(HR,HR.1),2),
+            ICC = round(ICC(data.frame(HR,HR.1))$results[2,2],2))
+
+formattable(df_sum)
+
+#make a bar plot
+##MAPE
+g<-ggplot(df_sum_act,aes(x=Type,y=MAPE))
+MAPE <- g + 
+  geom_bar(stat='identity', 
+                     position=position_dodge(),
+                     aes(fill=Color),
+                     color='black') + 
+  facet_grid(Activity~.) +
+  labs(y="MAPE(%)")
+MAPE
+##ICC
+g<-ggplot(df_sum_act,aes(x=Type,y=ICC))
+ICC <- g + 
+  geom_bar(stat='identity', 
+           position=position_dodge(),
+           aes(fill=Color),
+           color='black') + 
+  facet_grid(Activity~.) +
+  labs(y="ICC")+
+  geom_hline(yintercept=0.75,
+             linetype="dashed",
+             color="red")
+ICC
